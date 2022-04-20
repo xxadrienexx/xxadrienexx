24bf28873c3158d0abdcf5f79f4ff073cc2fb490
xxadrienexx <100779535+xxadrienexx@users.noreply.github.com>
2022-04-20 11:07:55 -0500

    Add files via upload

---
+++ Ejercicio 3 clase 19 abr.py
@@ -0,0 +1,10 @@
+#Escribir un programa que pregunte al usuario por el número de horas trabajadas
+#y el coste por hora.
+#Después debe mostrar por pantalla la paga que le corresponde.
+
+a = int(input ("Ingrese el numero de horas trabajadas: "))
+
+b = int(input ("Ingrese el valor por hora: "))
+
+print("El pago que le corresponde es: "+str(a*b))
+
